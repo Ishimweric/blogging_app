5483ed3a7361f42811107fb4b7bc2de706e9732c
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-31 13:46:07 +0200

    add: Finished signup users function

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/Users.js"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
+import { use } from "react";
 
 dotenv.config(); //load env variables
 
@@ -10,4 +11,47 @@ const generateToken = (id) =>{
   return jwt.sign({id}, process.env.JWT_SECRET, {
     expiresIn : "7d", // expires after seven days
   });
+};
+
+// signup users function
+const signupUser = async (req, res)=>{
+  const {username, email, password} = req.body;
+
+  //check if all are provided
+  if (!username || !email || !password){
+    return res.status(400).json({"Error" : "Please enter all fields"})
+  }
+
+  try {
+    // check if user already exists either by email or username
+    const userExists = await User.findOne({$or: [{email}, {username}]});
+    if (userExists){
+      return res.status(409).json({"message" : "user with this email or username already exists"})
+    }
+
+    //create a new user object
+    const user = await User.create({
+      username,
+      email,
+      password
+    });
+
+    // if user is successfully created
+    if (user){
+      //send back details and token
+      res.status(201).json({
+        "message" : "User created succesfully!",
+        "_id" : user._id,
+        "username" : user.username,
+        "email" : user.email,
+        "avatar" : user.avatar,
+        "token" : generateToken(user._id)
+      });
+    }else{
+      res.status(400).json({"error" : "invalid user data"})
+    }
+  }catch (err) {
+    console.error("signup error");
+    res.status(500).json({"message" : "server error during signing you up"})
+  }
 };
