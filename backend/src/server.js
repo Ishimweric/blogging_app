ec272984ed68aa108e343bff7a3c8da14d1d999c
Ishimwe Eric <ishimwee299@gmail.com>
2025-07-31 11:44:39 +0200

    add: Seted up initial server structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,9 @@
 import dotenv from "dotenv"
+import express from "express"
+
+const app = express();
+const PORT = 3500;
+
+app.listen(PORT, ()=>{
+  console.log(`SERVER LISTENING ON PORT ${PORT}`)
+});
