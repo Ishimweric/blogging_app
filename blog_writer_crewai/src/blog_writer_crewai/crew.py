e3f65d7f27c250feffa79be30896d9c83a06abcb
SenaKebe <152234422+SenaKebe@users.noreply.github.com>
2025-08-02 04:52:53 +0300

    careated_agents

--- blog_writer_crewai/src/blog_writer_crewai/crew.py
+++ blog_writer_crewai/src/blog_writer_crewai/crew.py
@@ -2,6 +2,9 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+from config.agents import trend_hunter, writer, editor, summarizer
+from config.tasks import discover_trend, write_blog, edit_blog, summarize_blog
+
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
