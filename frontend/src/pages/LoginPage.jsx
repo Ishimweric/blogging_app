2b767cbdce17a1b9185e0a69eb9e61987bc33182
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-02 10:08:05 +0200

    add: Seted up the basic structure of the login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,4 +1,15 @@
+import toast from "react-hot-toast"
+import { useNavigate } from "react-router-dom";
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("")
+  const [isLoading, setisLoading] = useState(false)
+
+  // to navigate to dashboard
+  const navigate = useNavigate();
+
+  // to handle form submission
+  const handleSubmit = async()=>{}
   return (
     <div>LoginPage</div>
   )
