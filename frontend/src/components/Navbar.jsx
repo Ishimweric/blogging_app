056aad86ce55d6ce5ab866deb2b69f3101db722f
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-02 17:25:50 +0200

    add: Conditionally rendered theme and menu toggles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Link } from "react-router-dom";
+import {Menu, Moon, Sun, X} from "lucide-react"
 
 const Navbar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -14,11 +15,25 @@ const Navbar = () => {
 
     return false
   })
+
+  const toggleTheme = ()=>{};
+  const toggleMobileMenu = ()=>{};
+
   return (
     <nav className="bg-white dark:text-white text-black dark:bg-gray-900 sticky top-0 shadow-md p-4 z-50 transition-colors duration-300 ease-in-out">
       <div className="container flex justify-between mx-auto items-center">
         {/* the logo */}
         <Link to={"/"} className="text-2xl text-gray-800 font-bold dark:text-white hover:text-gray-900 dark:hover:text-gray-200 transition-colors duration-200">NoteDown</Link>
+
+        {/* dark mode toggle and the mobile menu */}
+        <div className="flex items-center space-x-4">
+          <button onClick={toggleTheme} className="p-2 hover:bg-gray-200 rounded-full dark:hover:bg-gray-700 text-gray-700 dark:text-gray-300 transition-colors duration-200">
+            {isDarkmode? <Sun size={20}/> : <Moon size={20}/>}
+          </button>
+          <button onClick={toggleMobileMenu} className="text-gray-700 dark:text-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 hover:bg-gray-200 dark:hover:bg-gray-700  focus:ring-blue-500">
+            {isMobileMenuOpen? <X size={20}/> : <Menu size={20}/>}
+          </button>
+        </div>
       </div>
     </nav>
   )
