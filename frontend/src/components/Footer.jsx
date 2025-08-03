b48ec59a16c4cce43bd3b316a0bdd3d59a085eff
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-03 13:04:00 +0200

    added darkmode to the footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="text-sm text-gray-700 text-center fixed w-full bottom-0 bg-gray-100 py-2">&copy;{currentYear} NoteDown. All Rights Reserved</footer>
+    <footer className="text-sm text-gray-700 dark:text-gray-100 text-center fixed w-full bottom-0 bg-gray-100 dark:bg-gray-900 py-2">&copy;{currentYear} NoteDown. All Rights Reserved</footer>
   )
 }
 
