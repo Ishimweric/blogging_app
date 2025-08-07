7b569f29a715c06c00e8ec50efdda16b182294a7
Ishimwe Eric <ishimwee299@gmail.com>
2025-08-07 08:41:54 +0200

    fix(blogcard): Made blog summary adapts to darkmode

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -76,7 +76,7 @@ const BlogCard = ({ post, onSelectPost }) => {
           <span>{post.date}</span>
         </div>
 
-        <p className="text-sm text-gray-700">{post.summary}</p>
+        <p className="text-sm text-gray-700 dark:text-gray-100">{post.summary}</p>
 
         <div className="flex items-center gap-4 text-sm text-gray-500">
           <button 
